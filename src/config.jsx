ea8809d8fade105b7966f17dc467ada9dafc3798
support@prolianceltd.com <support@prolianceltd.com>
2025-03-19 08:43:13 +0100

    added the API host name

--- src/config.jsx
+++ src/config.jsx
@@ -2,18 +2,10 @@
 const config = {
 
 
- // API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
- //API_BASE_URL: 'http://44.205.44.84:8080',
  
- API_BASE_URL: 'http://127.0.0.1:9090',
-// API_BASE_URL: 'https://test.api.cmvp.net',
+ // API_BASE_URL: 'http://127.0.0.1:9090',
+API_BASE_URL: 'https://knowlege-base-api-rj3h.onrender.com',
 // WEB_PAGE_BASE_URL: 'https://cmvp.net',
-
-// WEB_PAGE_BASE_URL: 'https://cmvp-project.vercel.app',
-
-// WEB_PAGE_BASE_URL: 'http://localhost:5173',
-
- // ENDPOINT TO DELETE A USER SUBSCRIPTION: http://127.0.0.1:9090/api/subscription/auth/api/user-subscriptions/4/
 };
 
 export default config;
